1219193db77123e419e4e1786693ff3a2447a9af
tjddnghkrk <48312457+tjddnghkrk@users.noreply.github.com>
2020-04-03 21:39:07 +0900

    Update merge.c

--- merge.c
+++ merge.c
@@ -1,17 +1,8 @@
 
-void mergesort(int arr[], int p, int r) {
-    int q;
-    if(p<r) {
-        q = (p+r)/2;
-        mergesort(arr, p , q);
-        mergesort(arr, q+1, r);
-        merge(arr, p, q, r);
-    }
-}
 void merge(int arr[], int p, int q, int r) {
     int i = p, j = q+1, k = p;
 
-	int * tmp = (int*)malloc(sizeof(int)*num);
+	int * tmp = (int*)malloc(sizeof(int)*strlen(arr));
     
 	while(i<=q && j<=r) {
         if(arr[i] <= arr[j]) tmp[k++] = arr[i++];
@@ -25,5 +16,15 @@ void merge(int arr[], int p, int q, int r) {
 		arr[a] = tmp[a];
 
 	free(tmp);
+}
+
+void mergesort(int arr[], int p, int r) {
+    int q;
+    if(p<r) {
+        q = (p+r)/2;
+        mergesort(arr, p , q);
+        mergesort(arr, q+1, r);
+        merge(arr, p, q, r);
+    }
 }
 
